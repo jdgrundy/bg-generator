f8e4434606ebe084f1cb99819db7c1ce980669bc
jdgrundy <j.d.grundy22@gmail.com>
2024-07-17 15:46:53 +0100

    installed lodash locally, created bundle.js file

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+var _ = require("lodash");
+
+let anArray = [1, 2, 3, 4, 5, 6, 7];
+console.log(anArray);
+console.log("answer: ", _.without(anArray, 4));
+
 let css = document.querySelector("h3");
 let color1 = document.querySelector(".color1");
 let color2 = document.querySelector(".color2");
